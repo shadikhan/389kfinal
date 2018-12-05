bb54e41a740bb2a96ab63b6458a18f096f23ece8
Shad Khan <shad@wireless-10-105-59-177.umd.edu>
2018-12-05 05:21:28 -0500

    Fixed index.js

--- index.js
+++ index.js
@@ -195,6 +195,6 @@ app.post('/api/addRecipe', function (req, res) {
 });
 
 
-app.listen(3000, function () {
-    console.log('Final Project listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
